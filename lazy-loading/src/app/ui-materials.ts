c48aa5bfd1736d8039204859e99b0b827d37969f
Arsen Tabaku <arsen.tabaku@vrdirect.com>
2022-09-08 10:16:55 +0200

    Added materials from angular-material

---
+++ lazy-loading/src/app/ui-materials.ts
@@ -0,0 +1,25 @@
+import { MatSliderModule } from '@angular/material/slider';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+export const UI_MODULES = [
+  MatSliderModule,
+  MatBadgeModule,
+  MatTableModule,
+  MatFormFieldModule,
+  MatListModule,
+  MatSidenavModule,
+  MatRadioModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatMenuModule,
+  MatPaginatorModule,
+];
